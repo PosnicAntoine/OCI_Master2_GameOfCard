7c52598c7ede9367955ed045d0fbe64314927379
Dagaubs <alan181096@gmail.com>
2018-11-28 10:38:39 +0100

    Add title in Js to GameLoginPage

--- platforms/browser/www/js/joinGames.js
+++ platforms/browser/www/js/joinGames.js
@@ -2,7 +2,7 @@ $(window).on('load', init);
 
 function init(evt){
     var jeuxvalue = window.location.search.substring(1);
-    var jeu = jeuxvalue.substring(jeuxvalue.indexOf('=')+1);
+    var jeu = jeuxvalue.substring(jeuxvalue.indexOf('='));
     console.log("jeuxvalue :", jeu);
 
     $("#titleJoinGames").text(jeu);
