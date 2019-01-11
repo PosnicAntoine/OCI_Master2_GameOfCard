d04c4b58f2e3bbfaa4c23774db2395554ff82370
Vogan <jbbordier@hotmail.fr>
2019-01-11 15:11:49 +0100

    Add shuffle and deck creation

--- www/js/Picolo/GameManager.js
+++ www/js/Picolo/GameManager.js
@@ -1,6 +1,34 @@
 class GameManager{
-    Init(){
-        var deck = [];
-        filldeck();
+    constructor(){
+        this.deck = [];
+        filldeck(deck);
+        shuffle(deck);
     }
+    filldeck(){
+        for(var i=1 ;i<14 ;i++){
+            var card1 = new Card(i,'heart');
+            var card2 = new Card(i,'spade');
+            var card3 = new Card(i,'diamond');
+            var card4 = new Card(i,'club');
+            this.deck.push(card1);
+            this.deck.push(card2);
+            this.deck.push(card3);
+            this.deck.push(card4);
+        }
+    }
+     shuffle() {
+        for (let i = this.deck.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+        }
+        return a;
+     }
+     shift(){
+         return this.deck.shift();
+     }
+
+     getCard(){
+        var current = this.deck[0];
+        shift();
+     }
 }
