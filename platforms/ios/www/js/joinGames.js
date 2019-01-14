566b16f8ceb7912bd34af8f8950a57a17b70eb98
Vogan <jbbordier@hotmail.fr>
2019-01-14 09:18:01 +0100

    gitignore modif

--- platforms/ios/www/js/joinGames.js
+++
@@ -1,9 +0,0 @@
-$(window).on('load', init);
-
-function init(evt){
-    var jeuxvalue = window.location.search.substring(1);
-    var jeu = jeuxvalue.substring(jeuxvalue.indexOf('=')+1);
-    console.log("jeuxvalue :", jeu);
-
-    $("#titleJoinGames").text(jeu);
-}
